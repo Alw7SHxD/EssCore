d62ca9ed36c99a0826b5026f287482cdb03df32e
dotalw <15912596+dotalw@users.noreply.github.com>
2019-04-19 08:40:08 +0400

    added license, consoleOnly() and modified methods description

--- src/main/java/me/dotalw/esscore/lib/utils/command/Command.java
+++ src/main/java/me/dotalw/esscore/lib/utils/command/Command.java
@@ -1,3 +1,18 @@
+/*
+ *    Copyright (C) 2011-2019 dotalw.
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
 package me.dotalw.esscore.lib.utils.command;
 
 import java.lang.annotation.ElementType;
@@ -8,50 +23,54 @@ import java.lang.annotation.Target;
 @Target({ElementType.METHOD})
 @Retention(RetentionPolicy.RUNTIME)
 public @interface Command {
-
     /**
-     * Name of command
-     * @return
+     * Name of the command
+     * @return command name
      */
-
     public String name();
 
     /**
-     * Description of command - help of the cmd
-     * @return
+     * Description of the command
+     * @return command description
      */
-
     public String description() default "";
 
     /**
-     * Usage of command - /help (commandname)
-     *
-     * @return
+     * Usage of the command - /help (command)
+     * @return command usage
      */
     public String usage() default "";
+
     /**
      * Aliases of the command.
-     *
-     * @return
+     * @return command aliases
      */
     public String[] aliases() default {};
+
     /**
-     * Players or not only?
-     *
-     * @return
+     * Indicates whether the command shall be executed by players only.
+     * @return TRUE if the command is a player's only command
+     *         FALSE otherwise.
      */
     public boolean playerOnly() default false;
+
     /**
-     * Gets the required permission of the command
-     *
-     * @return
+     * Indicates whether the command shall be executed by the console only.
+     * @return TRUE if the command is a console command
+     *         FALSE otherwise.
+     */
+    public boolean consoleOnly() default false;
+
+    /**
+     * Gets the required permission of the command.
+     * @return command permission
      */
     public String permission() default "";
+
     /**
-     * When a player that doesnt have the permission tries to execute it.
+     * When a player doesnt have the permission tries to execute it.
      * Essentially, this will point to the configuration file.
-     *
-     * @return
+     * @return no permission message
      */
     public String noPerm() default "You do not have permission to perform that action";
 
